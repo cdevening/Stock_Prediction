27d9fb064f328b11e0b02d5dada6fa0807b82f25
cdevening <deveningcade@gmail.com>
2026-04-21 17:49:44 -0500

    Reorganize imports and update Streamlit app logic

--- Portfolio/StreamlitApp_HW6.py
+++ Portfolio/StreamlitApp_HW6.py
@@ -131,15 +131,15 @@ def display_explanation(input_df, session, aws_bucket):
     input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
     shap_values = explainer(input_df_transformed)
     
-st.subheader("🔍 Decision Transparency (SHAP)")
-fig, ax = plt.subplots(figsize=(10, 4))
-shap.plots.waterfall(shap_values[0], max_display=10)
-#shap.plots.waterfall(shap_values[0, :, 0])
-st.pyplot(fig)
-# top feature 
-top_feature = pd.Series(shap_values[0].values, index=shap_values[0].feature_names).abs().idxmax()
-#top_feature = pd.Series(shap_values[0, :, 0].values, index=shap_values[0, :, 0].feature_names).abs().idxmax()
-st.info(f"**Business Insight:** The most influential factor in this decision was **{top_feature}**.")
+    st.subheader("🔍 Decision Transparency (SHAP)")
+    fig, ax = plt.subplots(figsize=(10, 4))
+    shap.plots.waterfall(shap_values[0], max_display=10)
+    #shap.plots.waterfall(shap_values[0, :, 0])
+    st.pyplot(fig)
+    # top feature 
+    top_feature = pd.Series(shap_values[0].values, index=shap_values[0].feature_names).abs().idxmax()
+    #top_feature = pd.Series(shap_values[0, :, 0].values, index=shap_values[0, :, 0].feature_names).abs().idxmax()
+    st.info(f"**Business Insight:** The most influential factor in this decision was **{top_feature}**.")
 
 
 # Streamlit UI
