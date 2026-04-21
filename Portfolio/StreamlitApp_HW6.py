29fc1830cc0de7a3d0b205514ad0719cacf4fcbc
cdevening <deveningcade@gmail.com>
2026-04-21 17:48:40 -0500

    Update StreamlitApp_HW6.py

--- Portfolio/StreamlitApp_HW6.py
+++ Portfolio/StreamlitApp_HW6.py
@@ -108,14 +108,14 @@ def call_model_api(input_df):
 
     try:
         # For regression
-        # raw_pred = predictor.predict(input_df)
-        # pred_val = pd.DataFrame(raw_pred).values[-1][0]
-        # return round(float(pred_val), 4), 200
-        # For classification
         raw_pred = predictor.predict(input_df)
         pred_val = pd.DataFrame(raw_pred).values[-1][0]
-        mapping = {0: "SELL", 1: "HOLD", 2: "BUY"}
-        return mapping.get(pred_val), 200
+        return round(float(pred_val), 4), 200
+        # For classification
+        #raw_pred = predictor.predict(input_df)
+        #pred_val = pd.DataFrame(raw_pred).values[-1][0]
+        #mapping = {0: "SELL", 1: "HOLD", 2: "BUY"}
+        #return mapping.get(pred_val), 200
     except Exception as e:
         return f"Error: {str(e)}", 500
 
@@ -133,12 +133,12 @@ def display_explanation(input_df, session, aws_bucket):
     
 st.subheader("🔍 Decision Transparency (SHAP)")
 fig, ax = plt.subplots(figsize=(10, 4))
-#shap.plots.waterfall(shap_values[0], max_display=10)
-shap.plots.waterfall(shap_values[0, :, 0])
+shap.plots.waterfall(shap_values[0], max_display=10)
+#shap.plots.waterfall(shap_values[0, :, 0])
 st.pyplot(fig)
 # top feature 
-# top_feature = pd.Series(shap_values[0].values, index=shap_values[0].feature_names).abs().idxmax()
-top_feature = pd.Series(shap_values[0, :, 0].values, index=shap_values[0, :, 0].feature_names).abs().idxmax()
+top_feature = pd.Series(shap_values[0].values, index=shap_values[0].feature_names).abs().idxmax()
+#top_feature = pd.Series(shap_values[0, :, 0].values, index=shap_values[0, :, 0].feature_names).abs().idxmax()
 st.info(f"**Business Insight:** The most influential factor in this decision was **{top_feature}**.")
 
 
