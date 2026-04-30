7fe10f4d1030c178a974757488266e60792e09a0
cdevening <deveningcade@gmail.com>
2026-04-29 22:04:59 -0500

    Update StreamlitApp_Project.py

--- Portfolio/StreamlitApp_Project.py
+++ Portfolio/StreamlitApp_Project.py
@@ -120,7 +120,7 @@ def call_model_api(input_df):
         raw_pred = predictor.predict(input_df)
         pred_val = pd.DataFrame(raw_pred).values[-1][0]
         #mapping = {0: "SELL", 1: "HOLD", 2: "BUY"}
-        mapping = {0: "Legitimate", 1: "Fraud"}
+        mapping = {0: "Fully Paid / Lower Risk", 1: "Charged Off / Higher Risk"}
         return mapping.get(pred_val), 200
     except Exception as e:
         return f"Error: {str(e)}", 500
