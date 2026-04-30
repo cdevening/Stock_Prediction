aa486ea71d61f1a6ed504ff489f4502fef33e3e7
cdevening <deveningcade@gmail.com>
2026-04-30 17:43:31 -0500

    Update StreamlitApp_Project.py

--- Portfolio/StreamlitApp_Project.py
+++ Portfolio/StreamlitApp_Project.py
@@ -39,11 +39,6 @@ if project_root not in sys.path:
 #from src.feature_utils import extract_features
 #from src.Custom_Classes import DropHighMissingCols, TransactionFeatureEngineer, DropHighCorrelation
 
-file_path = os.path.join(project_root, 'Portfolio/X_train.csv')
-
-dataset = pd.read_csv(file_path)
-dataset = dataset.drop(['Unnamed: 0'],axis=1)
-#dataset = dataset.loc[:, ~dataset.columns.str.contains('^Unnamed')]
 
 # Access the secrets
 aws_id = st.secrets["aws_credentials"]["AWS_ACCESS_KEY_ID"]
